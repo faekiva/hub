9ad1583bbc0dee8dff7220fc7c08af1daf968020
Kiva <kivahilgenberg@gmail.com>
2023-12-14 20:50:36 -0500

    nuxt basic

---
+++ nuxt-app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+  <div>
+    
+  </div>
+  <NuxtPage />
+</template>
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+<!-- https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3 -->
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  </div>
+</template>
