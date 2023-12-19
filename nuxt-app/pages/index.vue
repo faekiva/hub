ba4c595025d0e76f18ad5f3537c53fec42cac1a2
Kiva <kivahilgenberg@gmail.com>
2023-12-18 23:52:02 -0500

    idx

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,45 +1,18 @@
 <script setup lang="ts">
 const route = useRoute()
+
+const posts = ref(["", ""])
 </script>
 
 <template>
   <!-- <p>Current route: {{ route.path }}</p> -->
   <GlobalHeader />
-  <div class="flex justify-center w-full">
-    <div class="card bg-base-100 w-1/3 shadow-xl">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 w-1/3 shadow-xl">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 w-1/3 shadow-xl">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="flex justify-center w-full">
-    <div class="stats bg-primary w-1/2 text-primary-content">
-      <textarea class="textarea textarea-ghost" placeholder="Bio"></textarea>
+  <p class="flex justify-center w-full">{{ posts }}</p>
+  <div class="flex justify-center w-full flex-wrap">
+
+    <div class="flex flex-wrap justify-center stats bg-primary w-1/2 text-primary-content">
+      <textarea v-for="(_, index) in posts" v-model="posts[index]"
+        class="flex flex-wrap justify-center textarea textarea-ghost w-full" placeholder="Bio"></textarea>
     </div>
   </div>
 </template>
